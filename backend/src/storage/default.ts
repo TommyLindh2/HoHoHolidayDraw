060647c3c55c768a6a0b6025cc260f490b13a5f8
Tommy Hallman Lindh <tommy.lindh@lime.tech>
2025-10-22 11:16:40 +0200

    style: better typing

--- backend/src/storage/default.ts
+++ backend/src/storage/default.ts
@@ -25,9 +25,6 @@ export function InitializeDefaultPersons(storage: DbStorage) {
             pictureUrl: profilesPrefix + "lena.jpg",
         },
         {
-            name: "Anders",
-        },
-        {
             name: "Tobias",
         },
         {
@@ -80,10 +77,11 @@ export function InitializeDefaultPersons(storage: DbStorage) {
     ];
 
     for (let i = 0; i < persons.length; i++) {
-        persons[i] = storage.CreatePerson(persons[i]);
+        persons[i] = storage.CreatePerson(persons[i] as models.Person);
     }
 
-    const groups: Array<Partial<models.Group> & { persons?: string[] }> = [
+    type GroupWithPersons = Partial<models.Group> & { persons?: string[] };
+    const groups: Array<GroupWithPersons> = [
         {
             name: "Lindharna",
             persons: [
@@ -92,7 +90,6 @@ export function InitializeDefaultPersons(storage: DbStorage) {
                 "Tina",
                 "Annie",
                 "Lena",
-                "Anders",
                 "Tobias",
                 "P-O",
                 "Annalena",
@@ -133,7 +130,7 @@ export function InitializeDefaultPersons(storage: DbStorage) {
     ];
 
     for (let i = 0; i < groups.length; i++) {
-        const group = groups[i];
+        const group = groups[i] as GroupWithPersons;
         const groupBelongings = group?.persons ?? [];
         delete group.persons;
 
